e204f5cc805642f7551e92eb8a2b5d4975522349
Joahn Aml <joahn_aml@outlook.com>
2023-05-31 11:52:37 +0200

    feat: add skeleton ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { AppBar, popup, RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	import 'iconify-icon';
+	import { languageStore } from '$lib/language.js';
+
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">Skeleton</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<div class="relative hidden lg:block">
+			<!-- trigger -->
+			<button
+				class="btn hover:variant-soft-primary"
+				use:popup={{ event: 'click', target: 'features' }}
+				><span>Explore</span><iconify-icon icon="mdi:caret-down" /></button
+			>
+			<!-- popup -->
+			<div class="card shadow-xl" data-popup="features">
+				<RadioGroup
+					active="variant-filled-secondary"
+					hover="hover:variant-soft-primary"
+					display="flex-col"
+				>
+					<RadioItem bind:group={$languageStore} name="justify" value={'english'}>English</RadioItem
+					>
+					<RadioItem bind:group={$languageStore} name="justify" value={'swedish'}>Svenska</RadioItem
+					>
+					<RadioItem bind:group={$languageStore} name="justify" value={'german'}>Deutsch</RadioItem>
+				</RadioGroup>
+			</div>
+		</div>
+	</svelte:fragment>
+</AppBar>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import { languageStore } from '$lib/language.js';
+</script>
+
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p class="text-tertiary-500">
+		The main reason for using lorem ipsum text is that it keeps people from focusing on the actual
+		text. When someone creates a template and asks for feedback on it, they don't want the people
+		reviewing it to get distracted by what the text says. Instead of focusing on the layout of the
+		text, people might read the words to look for typos or sing along to the song lyrics.
+	</p>
+	<section>
+		<RadioGroup active="variant-filled-secondary" hover="hover:variant-soft-primary">
+			<RadioItem bind:group={$languageStore} name="justify" value={'english'}>English</RadioItem>
+			<RadioItem bind:group={$languageStore} name="justify" value={'swedish'}>Svenska</RadioItem>
+			<RadioItem bind:group={$languageStore} name="justify" value={'german'}>Deutsch</RadioItem>
+		</RadioGroup>
+	</section>
+</div>
